78ef1b59a8b7785ba9152e1f6483019a53c294ec
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2023-08-23 20:00:17 -0600

    cambios tilinos

--- Parcial1.py
+++ Parcial1.py
@@ -7,7 +7,7 @@
 # librerias:
 from tkinter import * 
 from tkinter import messagebox
-import math
+from math import sin, cos, tan 
 from sympy import *  #libreria para las integrales
 
 class app(Tk):
@@ -23,7 +23,7 @@ class app(Tk):
         self.e1=Entry(self);self.e1.place(x=85,y=10)
 
         self.l2=Label(text="Largo:");self.l2.place(x=10,y=50); self.l2.config(bg="#ffb3c6")
-        self.e1=Entry(self);self.e1.place(x=85,y=50)
+        self.e2=Entry(self);self.e2.place(x=85,y=50)
         
         btn1= Button(self, text="Anillo", width=15,command=self.Anillo, bg="#ffc2d1");btn1.place(x=350,y=10)
         btn2=Button(self, text="Disco", width=15, command=self.Disco, bg="#ffb3c6");btn2.place(x=350,y=50)
@@ -32,7 +32,44 @@ class app(Tk):
 
 
     def Anillo(self):
-        pass
+        #calcular el campo electrico en el punto P del eje x
+        #deducir la ecuacion de E
+        #ecuaciones utiles: 
+        """
+        E = K * q/r^2
+        dE = K * dq/r^2
+        Cos= adyacente/hipotenusa = x/r
+        r= sqrt(a^2+x^2)
+        """
+   
+        #try: 
+        self.radio = float (self.e1.get())
+
+        if self.radio>0:
+            #variables a usar
+            self.constanteK= 9*10**9
+            #campo = E, dE= DiferencialDeCampo, dq= DiferencialDeCarga, a= radio, r= distancia de diferencial de carga al punto, x= distancia
+            #self.Campo, self.DiferencialDeCampo, self.DiferencialDeCarga, self.r, self.x, self.a, self.Carga
+            self.r=-1
+            #deduccion
+            #derivada del campo dE
+            self.Carga= symbols('self.Carga')
+            self.diferencialDeCarga= symbols('dq') #diferencial de carga
+            self.constanteK, self.r = symbols('constanteK r', positive=True, real=True)
+            self.Campo= self.constanteK * (self.Carga/pow(self.r,2))
+            self.DiferencialDeCampo= diff(self.Campo, self.Carga)* self.diferencialDeCarga
+           
+           
+            #self.r= sqrt(pow(self.radio,2)+pow(self.x,2))
+            
+            
+            messagebox.showinfo("dE:",self.DiferencialDeCampo)
+
+        else:
+            pass    
+
+        #except Exception as msg: 
+        #    messagebox.showerror("error", "ingrese un número valido")
 
     def Disco(self):
         pass
